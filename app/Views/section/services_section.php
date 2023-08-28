5f214917c48ab0e1cdf7d398c0573f7d1bafcdbf
IBJD <ibjanardana@gmail.com>
2023-08-28 15:33:50 +0800

    update
    update services_section

--- app/Views/section/services_section.php
+++ app/Views/section/services_section.php
@@ -11,56 +11,73 @@
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Kintamani Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:00 AM<br><b>Price : USD $68 per person</b></p>
+              <p>Duration 9 Hours<br>Start: 08:00 AM<br><b>Price : USD $ 54 per person</b><br>
+              <b>IDR : Rp 800.000 per person</b></p>
               <ul>
                 <li>Barong Dance & Kriss Dance</li>
                 <li>Batuan Village Temple </li>
                 <li>Tirta Empul Temple</li>
+                <li>Elephant Cave Temple</li>
                 <li>Mount Batur & Volcano</li>
                 <li>Batur Lake</li>
                 <li>Lunch at a Local Restourant</li>
                 <li>Agrotourism & Coffee Plantation</li>
                 <li>Bali Art Handmade Center & Souvenir</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Kintamani%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-lg-0">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Ubud Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $76 per person</b></p>
+              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $ 62 per person</b><br>
+              <b>IDR : Rp 930.000 per person</b></p>
               <ul>
                 <li>Monkey Forrest</li>
                 <li>Ubud Palace</li>
+                <li>Ubud Traditional Market</li>
                 <li>Elephant Cave Temple</li>
                 <li>Lunch at a Local Restourant</li>
                 <li>Tegallalang Rice Terace</li>
                 <li>Swing</li>
                 <li>Agrotourism & Coffee Plantation</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Ubud%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Nusa Dua Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $99 per person</b></p>
+              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $ 113 per person</b><br>
+              <b>IDR : Rp 1.690.000 per person</b></p>
               <ul>
                 <li>Tanjung Benoa Water Sport Center</li>
+                <li>Banana Boat</li>
                 <li>Culture Park The GWK</li>
                 <li>Padang Padang Beach</li>
+                <li>Pandawa Beach</li>
                 <li>Lunch at a Local Restourant</li>
                 <li>Uluwatu Rock Clip Temple</li>
                 <li>Kecak & Fire Dance</li>
                 <li>Jimbaran Beach & Dinner</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Nusa%20Dua%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Bedugul Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $65 per person</b></p>
+              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $ 55 per person</b><br>
+              <b>IDR : Rp 820.000 per person</b></p>
               <ul>
                 <li>Taman Ayun Temple</li>
                 <li>Ulun Danu Beratan Lake Temple</li>
@@ -70,42 +87,55 @@
                 <li>Jati Luwih Rice Terrace</li>
                 <li>Tanah Lot Temple</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Bedugul%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Singaraja Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $68 per person</b></p>
+              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $ 45 per person</b><br>
+              <b>IDR : Rp 650.000 per person</b></p>
               <ul>
                 <li>Gitgit Waterfall</li>
                 <li>Lovina Beach</li>
                 <li>Lunch at a Local Restourant</li>
-                <li>Banjar Natural HotSpring Water</li>
+                <li>Budhis Temple</li>
+                <li>Banjar Natural Hot Spring Water</li>
                 <li>Munduk Hill Side & Coffee Plantation</li>
                 <li>Lake Buyan & Tamblingan Scanery</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Singaraja%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Penglipuran Tour</a></h4>
-              <p>Duration 8 Hours<br>Start: 08:30 AM<br><b>Price: USD $65 per person</b></p>
+              <p>Duration 8 Hours<br>Start: 08:30 AM<br><b>Price: USD $ 52 per person</b><br>
+              <b>IDR : Rp 780.000 per person</b></p>
               <ul>
                 <li>Penglipuran Village</li>
                 <li>Tibumana Waterfall</li>
                 <li>Kanto Lampo Waterfall</li>
                 <li>Lunch at Local Restourant</li>
-                <li>Elephant Cave Temple</li>
+                <li>Bat Cave Temple</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Penglipuran%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">North Nature Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price: USD $65 per person</b></p>
+              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price: USD $ 56 per person</b><br>
+              <b>IDR : Rp 820.000 per person</b></p>
               <ul>
                 <li>Ulun Danu Beratan Temple</li>
                 <li>Lake Buyan & Tamblingan</li>
@@ -114,67 +144,107 @@
                 <li>Munduk Coffee Plantation</li>
                 <li>Pupuan Rice Terace & Nature View</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20North%20Nature%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
             <div class="col-md-6 mt-4">
               <div class="icon-box">
                 <h4><a href="#">Nusa Penida West Tour</a></h4>
-                <p>Duration 9 Hours<br>Start: 06:30 AM Pick Up an Hotel<br><b>Price: USD $120 per person</b></p>
+                <p>Duration 9 Hours<br>Start: 06:30 AM Pick Up an Hotel<br><b>Price: USD $120 per person</b><br>
+                <b>IDR : Rp 1.800.000 per person</b></p>
                 <ul>
                   <li>Klingking Beach</li>
                   <li>Broken Beach</li>
                   <li>Angle Billabong Beach</li>
                   <li>Crystal Beach</li>
                 </ul>
+                <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Nusa%20Penida%20West%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 mt-4">
+              <div class="icon-box">
+                <h4><a href="#">Nusa Penida East Tour</a></h4>
+                <p>Duration 9 Hours<br>Start: 06:30 AM Pick Up an Hotel<br><b>Price: USD $ 125 per person</b><br>
+                <b>IDR : Rp 1.875.000 per person</b></p>
+                <ul>
+                  <li>Diamond Beach</li>
+                  <li>Atuh Beach</li>
+                  <li>Tree House</li>
+                  <li>King Five</li>
+                </ul>
+                <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Nusa%20Penida%20East%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Dolphin Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 02:30 AM<br><b>Price : USD $60 per person</b><br>Daily Start Early Morning at Lovina Beach North of Bali</p>
+              <p>Duration 9 Hours<br>Start: 02:30 AM<br><b>Price : USD $ 50 per person</b><br>
+              <b>IDR : Rp 740.000 per person</b><br>Daily Start Early Morning at Lovina Beach North of Bali</p>
               <ul>
                 <li>Hot Natural Spring Water</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Dolphin%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Bali Zoo Park Tour</a></h4>
-              <p>Duration 8 Hours<br>Start: 08:30 AM<br><b>Price : USD $140 per person</b></p>
+              <p>Duration 8 Hours<br>Start: 08:30 AM<br><b>Price : USD $ 150 per person</b><br>
+              <b>IDR : Rp 2.250.000 per person</b></p>
               <ul>
                 <li>Bali Safari Marine Park</li>
                 <li>Bali Bird Park</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Bali%20Zoo%20Park%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
 
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
-              <h4><a href="#">Besakih Temple Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $69 per person</b></p>
+              <h4><a href="#">Besakih Tour</a></h4>
+              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $ 60 per person</b><br>
+              <b>IDR : Rp 900.000 per person</b></p>
               <ul>
                 <li>The Besakih Mother Temple of Bali</li>
                 <li>Tirta Gangga Water Palace</li>
                 <li>Lunch at a Local Restourant</li>
                 <li>Lempuyang Gate</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Besakih%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
 
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
-              <h4><a href="#">East Tour Lempuyang</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $68 per person</b></p>
+              <h4><a href="#">East Lempuyang Tour</a></h4>
+              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $ 65 per person</b><br>
+              <b>IDR : Rp 970.000 per person</b></p>
               <ul>
                 <li>Goa Lawah Cave Temple</li>
+                <li>Tenganan Village</li>
                 <li>Tirta Gangga Water Palace</li>
                 <li>Lunch at a Local Restourant</li>
                 <li>Lempuyang Gate of Heaven</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20East%20Lempuyang%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
 
@@ -182,7 +252,8 @@
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">Tegallalang Tour</a></h4>
-              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $75 per person</b></p>
+              <p>Duration 9 Hours<br>Start: 08:30 AM<br><b>Price : USD $ 72 per person</b><br>
+              <b>IDR : Rp 1.040.000 per person</b></p>
               <ul>
                 <li>Rice Terrace Tegallalang</li>
                 <li>Swing</li>
@@ -191,6 +262,9 @@
                 <li>Tirta Empul Holy Water Temple</li>
                 <li>Tegunungan Waterfall</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Tegallalang%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
 
@@ -198,34 +272,48 @@
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#">JatihLuwih Tour</a></h4>
-              <p>Duration 8 Hours<br>Start: 08:30 AM<br><b>Price: USD $60 per person</b></p>
+              <p>Duration 8 Hours<br>Start: 08:30 AM<br><b>Price: USD $ 54 per person</b><br>
+              <b>IDR : Rp 810.000 per person</b></p>
               <ul>
                 <li>Taman Ayun Temple</li>
+                <li>Batu Karu Temple</li>
                 <li>JatihLuwih Best View Rice Terrace</li>
                 <li>Lunch at a Local Restourant</li>
                 <li>Tanah Lot Temple & Sunset</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20JatihLuwih%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
               <h4><a href="#"></a>Adventure Tour</h4>
-              <p>Duration 9 Hours<br>Start: 08:00 AM<br><b>Price: USD $260 per person</b></p>
+              <p>Duration 9 Hours<br>Start: 08:00 AM<br><b>Price: USD $ 197 per person</b><br>
+              <b>IDR : Rp 2.950.000 per person</b></p>
               <ul>
                 <li>White Water Rafting</li>
                 <li>Lunch</li>
                 <li>ATV Ride</li>
+                <li>Swing</li>
                 <li>Elephant Ride</li>
               </ul>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Adventure%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
           <div class="col-md-6 mt-4">
             <div class="icon-box">
               <i class="bi bi-card-checklist"></i>
-              <h4><a href="#"></a>Mount Batur Tracking Tour</h4>
-              <p>Duration 9 Hours<br>Start: 02:00 AM<br><b>Price: USD $80 per person</b></p>
+              <h4><a href="#"></a>Mount Batur Trekking Tour</h4>
+              <p>Duration 9 Hours<br>Start: 02:00 AM<br><b>Price: USD $ 80 per person</b><br>
+              <b>IDR : Rp 1.200.000 per person</b></p>
               <p>start early morning from the hotel at arround 02:00 am and,<br>2 hours drive on car to the start point at the mount of batur<br>it be at 04:00 am starting to climbing.</p>
+              <div class="d-grip gap-2 col-3 mx-auto">
+                <a href="https://wa.me/6282236171383?text=Hi%20i%20am%20interesting%20Mount%20Batur%20Tracking%20Tour" class="btn btn-primary">Book Now</a>
+              </div>
             </div>
           </div>
 
@@ -239,7 +327,8 @@
                  <li>Tickets for Attractions and Activities According to The Tour Program Above, Lunch or Dinner, Mineral Water.</li>
                  <li>The Price Rate of The Dollar Will Be Adjusted to The Price at That Time.</li> 
                  <li>Minimum 2 Person Tour Participant, For Single Traveling, an Additional Transportation Fee Will Be Charged.</li>
-                 <li>For a Time The Price Can Change.</li>
+                 <li>For a Time The Price Can Change and Will be Recalculated According to the Situation and Conditions.</li>
+                 <li>Before the Tour Package Event Starts Payment Must be Completed.</li>
                 </ul>
             </div>
           </div>
@@ -257,7 +346,7 @@
             <i class="bi bi-card-checklist"></i>
             <h4><a href="#"></a>Private Full Day Tour</h4>
             <p>
-            We are Organize Private Tour Bali<br>
+            <b>We are Organize Private Tour Bali</b><br>
             Whatsapp : <a href="https://wa.me/6282236171383">Chat Me</a><br>
             Our Website : <a href="https://balialwaystransport.com">Click Me</a></p>
           </div>
